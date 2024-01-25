98bd964c474b54b03effedcacac24f325f775d10
xJustFox <dodydaversa@gmail.com>
2024-01-25 14:55:28 +0100

    [Upgrade] Including var.php file, created table with $hotels data

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
+include __DIR__ . '/partials/var.php';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,9 +13,36 @@
     <link rel="stylesheet" href="./style/style.css">
     <title>PHP Hotel</title>
 </head>
+
 <body>
     <main>
+        <div class="container-lg">
 
+            <table class="table table-bordered border-primary text-center">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal centro</th>
+                        <th scope="col">Parcheggio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($hotels as $hotel) { ?>
+                        <tr>
+                            <td><?php echo $hotel['name'] ?></td>
+                            <td><?php echo $hotel['description'] ?></td>
+                            <td><?php echo $hotel['vote'] ?></td>
+                            <td><?php echo $hotel['distance_to_center'] ?></td>
+                            <td><?php echo $hotel['parking']; ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+
+            </table>
+        </div>
     </main>
 </body>
+
 </html>
